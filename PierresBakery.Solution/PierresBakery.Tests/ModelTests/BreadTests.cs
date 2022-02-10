ca966a270218466341b5b184165e836f4b930a6c
Patrick Dolan <dolanp1992@gmail.com>
2022-02-10 11:17:11 -0800

    rename and add files from/to my template for pierres bakery project

---
+++ PierresBakery.Solution/PierresBakery.Tests/ModelTests/BreadTests.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PierresBakery.Models;
+
+namespace PierresBakery.Tests
+{
+  [TestClass]
+  public class BreadTests
+  {
+    // Test methods go here
+  }
+}
+// Example Test Method Structure
+//_________________________
+// [TestMethod]
+// public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
+// {
+//   // any necessary logic to prep for test; instantiating new classes, etc.
+//   Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
+// }
