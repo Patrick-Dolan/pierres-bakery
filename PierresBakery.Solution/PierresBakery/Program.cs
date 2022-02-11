851c9598ff79d93c4c940eb301d197dde77be62c
Patrick Dolan <dolanp1992@gmail.com>
2022-02-11 15:32:42 -0800

    change spacing to make code easier to read

--- PierresBakery.Solution/PierresBakery/Program.cs
+++ PierresBakery.Solution/PierresBakery/Program.cs
@@ -9,11 +9,14 @@ namespace PierresBakery
     {
       Console.WriteLine("Welcome to Pierre's Bakery!");
       Console.WriteLine("===========================");
+      
       Console.WriteLine("How many loaves of bread would you like? (Buy 2, get 1 free)");
       Bread breadOrder = new Bread(Convert.ToInt32(Console.ReadLine()));
+
       Console.WriteLine("How many Pastries would you like? (Buy 2, get a 3rd half off)");
       Pastry pastryOrder = new Pastry(Convert.ToInt32(Console.ReadLine()));
-      Console.WriteLine("So you want " + breadOrder.NumberOfLoaves + " loaves of bread and " + pastryOrder.NumberOfPastries + " pastries. That will cost you: $" + (pastryOrder.PastryCost + breadOrder.BreadCost));
+
+      Console.WriteLine("You want " + breadOrder.NumberOfLoaves + " loaves of bread and " + pastryOrder.NumberOfPastries + " pastries. That will cost: $" + (pastryOrder.PastryCost + breadOrder.BreadCost));
     }
   }
 }
