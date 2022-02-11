2e9c157ad36fbfae0b0c3d77f750a3733a28370c
Patrick Dolan <dolanp1992@gmail.com>
2022-02-11 15:07:29 -0800

    add basic structure of program in console

--- PierresBakery.Solution/PierresBakery/Program.cs
+++ PierresBakery.Solution/PierresBakery/Program.cs
@@ -7,7 +7,13 @@ namespace PierresBakery
   {
     public static void Main()
     {
-      
+      Console.WriteLine("Welcome to Pierre's Bakery!");
+      Console.WriteLine("===========================");
+      Console.WriteLine("How many loaves of bread would you like? (Buy 2, get 1 free)");
+      Bread breadOrder = new Bread(Convert.ToInt32(Console.ReadLine()));
+      Console.WriteLine("How many Pastries would you like? (Buy 2, get a 3rd half off)");
+      Pastry pastryOrder = new Pastry(Convert.ToInt32(Console.ReadLine()));
+      Console.WriteLine("So you want " + breadOrder.NumberOfLoaves + " loaves of bread and " + pastryOrder.NumberOfPastries + " pastries. That will cost you: $" + (pastryOrder.PastryCost + breadOrder.BreadCost));
     }
   }
 }
