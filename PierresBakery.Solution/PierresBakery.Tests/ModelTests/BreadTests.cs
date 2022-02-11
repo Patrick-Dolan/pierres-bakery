4cf9c8ece1d2c3906ad38fb50d29f48a224fde00
Patrick Dolan <dolanp1992@gmail.com>
2022-02-11 13:41:57 -0800

    add test for getting number of loaves

--- PierresBakery.Solution/PierresBakery.Tests/ModelTests/BreadTests.cs
+++ PierresBakery.Solution/PierresBakery.Tests/ModelTests/BreadTests.cs
@@ -12,6 +12,15 @@ namespace PierresBakery.Tests
       Bread newBread = new Bread(4);
       Assert.AreEqual(typeof(Bread), newBread.GetType());
     }
+    [TestMethod]
+    public void GetNumberOfLoaves_ReturnNumberOfLoaves_Int()
+    {
+      int numberOfLoaves = 2;
+      Bread newBread = new Bread(numberOfLoaves);
+      int result = newBread.NumberOfLoaves;
+      Assert.AreEqual(numberOfLoaves, result);
+    }
+
   }
 }
 // Example Test Method Structure
