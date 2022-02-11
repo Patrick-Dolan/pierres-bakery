197e707b831f323a2523a97391cd0feb1db11a53
Patrick Dolan <dolanp1992@gmail.com>
2022-02-11 15:48:08 -0800

    add exception handling to numbers that cant be converted from strings

--- PierresBakery.Solution/PierresBakery/Program.cs
+++ PierresBakery.Solution/PierresBakery/Program.cs
@@ -9,14 +9,32 @@ namespace PierresBakery
     {
       Console.WriteLine("Welcome to Pierre's Bakery!");
       Console.WriteLine("===========================");
-      
+
       Console.WriteLine("How many loaves of bread would you like? (Buy 2, get 1 free)");
-      Bread breadOrder = new Bread(Convert.ToInt32(Console.ReadLine()));
+      string breadUserInput = Console.ReadLine();
+      int breadNumber;
+      if (!int.TryParse(breadUserInput, out breadNumber))
+      {
+        Console.WriteLine("Please input all orders as numbers. Example: 1, 2, 3...");
+        Console.WriteLine("Type enter to restart.");
+        Console.ReadLine();
+        Main();
+      }
 
       Console.WriteLine("How many Pastries would you like? (Buy 2, get a 3rd half off)");
-      Pastry pastryOrder = new Pastry(Convert.ToInt32(Console.ReadLine()));
+      string pastryUserInput = Console.ReadLine();
+      int pastryNumber;
+      if (!int.TryParse(pastryUserInput, out pastryNumber))
+      {
+        Console.WriteLine("Please input all orders as numbers. Example: 1, 2, 3...");
+        Console.WriteLine("Type enter to restart.");
+        Console.ReadLine();
+        Main();
+      }
 
-      Console.WriteLine("You want " + breadOrder.NumberOfLoaves + " loaves of bread and " + pastryOrder.NumberOfPastries + " pastries. That will cost: $" + (pastryOrder.PastryCost + breadOrder.BreadCost));
+      Bread breadOrder = new Bread(breadNumber);
+      Pastry pastryOrder = new Pastry(pastryNumber);
+      Console.WriteLine($"You want {breadOrder.NumberOfLoaves} loaves of bread and {pastryOrder.NumberOfPastries} pastries. That will cost: $" + (pastryOrder.PastryCost + breadOrder.BreadCost));
     }
   }
 }
